907fccfa75a45484ebadbc2c9c478c437a842158
hpaasch <hepaasch@gmail.com>
2016-06-28 13:56:43 -0400

    migrations made.

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from movie_app.models import Movie, Rating, Rater
+
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Rating)
+admin.site.register(Rater)
